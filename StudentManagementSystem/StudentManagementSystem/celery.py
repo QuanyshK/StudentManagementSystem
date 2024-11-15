5dc3a2c4a967de8c3e5a9bb2348fd0e140ad0bb7
QuanyshK <zoomer.qurbany@gmail.com>
2024-11-15 12:19:32 +0500

    Update

--- StudentManagementSystem/StudentManagementSystem/celery.py
+++ StudentManagementSystem/StudentManagementSystem/celery.py
@@ -1,9 +1,17 @@
-from __future__ import absolute_import
+# StudentManagementSystem/celery.py
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentManagementSystem.settings')
 
-app = Celery('student_management_system')
+app = Celery('StudentManagementSystem')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
